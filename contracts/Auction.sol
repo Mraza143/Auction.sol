918cc5546c180eff1c5e83d7a47d9f20893dffb8
Raza <mraza6601@gmail.com>
2022-10-01 22:28:32 +0500

    Struct made for Auction

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -9,7 +9,24 @@ error Auction__TransferFailed();
 
 contract Auction is  KeeperCompatibleInterface {
 
+    //mapping from a nft(adress + token Id) to a Auction
+    mapping(address => mapping(uint256 => Auction)) public nftContractAuctions;
+    
+    
+     struct Auction {
 
+        uint32  i_interval; // For How much time does the nft seller want the auction to continue
+        uint128 minPrice; // The price of the nft  at which the auction will start
+        uint256 s_lastTimeStamp; //The time at which the auction will start
+        address payable[] s_bidders; // The colletion of all the adresses which have made a bid for the nft
+        mapping(address => uint256) s_adressesToBid; //A mapping of all the addresses to their bid , so we can return their amount in case their bid did not win nft
+        mapping(address => uint256) s_addressToAmountFunded; // A mapping to receive bids
+        uint256 temporaryHighestBid; // The highest bid made for a nft at any given moment
+        uint128 nftHighestBid; //The bid which won the nft
+        address payable currentWinner; //The adress which is currently winning the auction , at the end of the auction , this will automatically get set to the final winner
+        address nftSeller; // The address of the seller of the nft
+        bool auctionStarted; // A bool to keep track whether the auction has started or not ;
+    }
     enum AuctionState {
         OPEN,
         CLOSE
@@ -19,17 +36,17 @@ contract Auction is  KeeperCompatibleInterface {
     /* Type declarations */
     
     /* State variables */
-    uint256 public constant i_originalPrice = 0.01 ether;
-    uint256 public  temporaryHighestBid;
-    uint256 public returnLoser;
-    mapping(address => uint256) public s_adressesToBid;
-    address payable[] public s_bidders;
-    address payable public currentWinner;
+    //uint256 public constant i_originalPrice = 0.01 ether;
+    //uint256 public  temporaryHighestBid;
+    //uint256 public returnLoser;
+    //mapping(address => uint256) public s_adressesToBid;
+    
+    //address payable public currentWinner;
     //address payable public seller;
-    bool public auctionStarted = false;
-    AuctionState private s_auctionState = AuctionState.CLOSE;
-    uint256 private immutable i_interval = 180;
-    uint256 private s_lastTimeStamp;
+    //bool public auctionStarted = false;
+    //AuctionState private s_auctionState = AuctionState.CLOSE;
+    //uint256 private immutable i_interval = 180;
+    //uint256 private s_lastTimeStamp;
     mapping(address => uint256) private s_addressToAmountFunded;
 
 

c66ed3e9b466cc312e16d5925a162fd1ef13d949
Raza <mraza6601@gmail.com>
2022-10-02 14:06:20 +0500

    added mock for erc721 via @openzeppelin/contracts

---
+++ contracts/Mocks/ERC721Mock.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mock is ERC721 {
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
